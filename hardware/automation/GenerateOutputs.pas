f611decca4d60cfd3d640e1132ec184ea5ebe76c
Kacper Jelonek <58530561+semitechen@users.noreply.github.com>
2026-05-21 02:25:48 +0200

    debug: fix command quoting and move log to C:\ root

--- hardware/automation/GenerateOutputs.pas
+++ hardware/automation/GenerateOutputs.pas
@@ -7,14 +7,13 @@ var
     LogFile : TextFile;
     LogPath : String;
 begin
-    LogPath := 'C:\Users\Omen\Desktop\altium_automation_log.txt';
+    LogPath := 'C:\altium_automation_log.txt';
     AssignFile(LogFile, LogPath);
     Rewrite(LogFile);
-    WriteLn(LogFile, 'Script started at ' + DateTimeToStr(Now));
+    WriteLn(LogFile, 'Script entry point reached at ' + DateTimeToStr(Now));
 
     Workspace := GetWorkspace;
     
-    // Try to find project
     Project := Workspace.DM_FocusedProject;
     if Project = nil then
     begin
@@ -22,13 +21,13 @@ begin
         if Workspace.DM_ProjectCount > 0 then
         begin
             Project := Workspace.DM_Projects(0);
-            WriteLn(LogFile, 'Found project in workspace: ' + Project.DM_ProjectFileName);
+            WriteLn(LogFile, 'Using first project in workspace: ' + Project.DM_ProjectFileName);
         end;
     end;
 
     if Project = nil then 
     begin
-        WriteLn(LogFile, 'Error: No project found. Exiting.');
+        WriteLn(LogFile, 'Error: No project found in workspace. Ensure the .PrjPcb was opened.');
         CloseFile(LogFile);
         Exit;
     end;
@@ -36,30 +35,29 @@ begin
     WriteLn(LogFile, 'Using project: ' + Project.DM_ProjectFileName);
 
     OutJobPath := Project.DM_ProjectDirectory + '\Default.OutJob';
-    WriteLn(LogFile, 'Looking for OutJob: ' + OutJobPath);
+    WriteLn(LogFile, 'Targeting OutJob: ' + OutJobPath);
 
     Document := Client.OpenDocument('OUTPUTJOB', OutJobPath);
     
     if Document <> nil then
     begin
-        WriteLn(LogFile, 'OutJob opened successfully.');
+        WriteLn(LogFile, 'OutJob opened. Starting generation...');
         Client.ShowDocument(Document);
         
-        WriteLn(LogFile, 'Starting generation...');
         ResetParameters;
         AddStringParameter('ObjectKind', 'OutputBatch');
         AddStringParameter('Action', 'Run');
         RunProcess('WorkSpaceManager:GenerateOutputs');
         
-        WriteLn(LogFile, 'Generation finished.');
+        WriteLn(LogFile, 'Generation process triggered.');
         Client.CloseDocument(Document);
     end
     else
     begin
-        WriteLn(LogFile, 'Error: Could not open OutJob file.');
+        WriteLn(LogFile, 'Error: Default.OutJob not found or could not be opened.');
     end;
     
-    WriteLn(LogFile, 'Terminating Altium.');
+    WriteLn(LogFile, 'Process complete. Terminating instance.');
     CloseFile(LogFile);
     Terminate;
 end;
